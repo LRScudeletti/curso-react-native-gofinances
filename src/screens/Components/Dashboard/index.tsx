b14a79049747ff0675e8ca383c26696968f06a11
Luiz Rogério Scudeletti <luiz.scudeletti@upper.net.br>
2024-01-22 13:32:33 -0300

    Adicionado componente de Header

--- src/screens/Components/Dashboard/index.tsx
+++ src/screens/Components/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
 
-import {Container, Title} from './styles'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  Username
+} from './styles'
 
 export function Dashboard() {
-    return (
-        <Container>
-            <Title>Dashboard</Title>
-        </Container>
-    )
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <View>
+            <UserInfo>
+              <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/48210960?v=4' }} />
+              <User>
+                <UserGretting>Olá,</UserGretting>
+                <Username>Luiz Rogério</Username>
+              </User>
+            </UserInfo>
+          </View>
+        </UserWrapper>
+      </Header>
+    </Container>
+  )
 }
